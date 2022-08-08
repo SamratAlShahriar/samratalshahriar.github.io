618d3ac12a76355ef6a018cb93c00ba036e80136
Samrat Al Shahriar <32227996+SamratAlShahriar@users.noreply.github.com>
2022-08-08 17:11:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY >20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass('sticky')
-        } else{
+        } else {
             $('.navbar').removeClass('sticky')
         }
+
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass('show')
+        } else {
+            $('.scroll-up-btn').removeClass('show')
+        }
     });
+
+    //slide up script
+    $('.scroll-up-btn').click(function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+
     //togle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active")
         $('.menu-btn i').toggleClass("active")
+    });
+
+    var workList = ["Android Developer", "Blogger", "Designer"];
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: workList,
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing2", {
+        strings: workList,
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 200,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
     });
 });
